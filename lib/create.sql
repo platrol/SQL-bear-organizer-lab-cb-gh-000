04e346e4b1de80069b191d19a09278241f2a3004
norman lewis <takialog@gmail.com>
2018-03-05 00:58:47 +0000

    automatically backed up by learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,9 +1,9 @@
 CREATE TABLE bears (
-  id INTIGER PRIMARY KEY,
+  id INTEGER PRIMARY KEY,
   name  TEXT,
-  age INTIGER,
+  age INTEGER,
   gender CHAR(1),
   color TEXT,
   temperament TEXT,
-  alive BOOOLEN
+  alive BOOLEAN
 )
