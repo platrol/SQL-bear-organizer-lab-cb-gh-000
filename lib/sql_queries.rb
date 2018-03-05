b709db341ad243ae907761864ab439f228790c92
norman lewis <takialog@gmail.com>
2018-03-05 01:20:03 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -1,5 +1,5 @@
 def selects_all_female_bears_return_name_and_age
-  "SELECT name age FROM bears WHERE gender = F;"
+  "SELECT name age FROM bears WHERE gender = 'F';"
 end
 
 def selects_all_bears_names_and_orders_in_alphabetical_order
@@ -19,11 +19,11 @@ def select_youngest_bear_and_returns_name_and_age
 end
 
 def selects_most_prominent_color_and_returns_with_count
-   "SELECT COUNT(color)   WHERE color = Blue"
+   "SELECT COUNT(color)   WHERE color = 'Blue'"
 end
 
 def counts_number_of_bears_with_goofy_temperaments
-  "SELECT COUNT(temperament) WHERE temperament = goofy;"
+  "SELECT COUNT(temperament) WHERE temperament = 'goofy';"
 end
 
 def selects_bear_that_killed_Tim
