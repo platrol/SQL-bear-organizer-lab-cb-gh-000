38a894d12b6a7fc98b23c71a0fdd95d0aa1ee491
norman lewis <takialog@gmail.com>
2018-03-05 01:03:52 +0000

    automatically backed up by learn

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name) VALUES
+        ("Mr.Chocolate"),
+        ('Rowdy'),
+        ('Sergeant Brown'),
+        ('Melissa'),
+        ('Ginch'),
+        ('Wwndy'),
+        (NULL)
+);
